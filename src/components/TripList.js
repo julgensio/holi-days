6fd195fd74a93c1b85dc059c6a0a7878a5a79124
Crunk <jb10051075@gmail.com>
2021-12-18 17:00:57 +0100

    Useeffect Dependency array
    Apply useEffect Dependency array for fetching data.

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -5,13 +5,15 @@ import './styles/TripList.css';
 
 export default function TripList() {
 	const [trips, setTrips] = useState([]);
+	const [url, setUrl] = useState('http://localhost:3000/trips');
 
 	// * Fetch data when TripList components first runs (use useEffect)
+	// ! function runs at the start and when the dependency [] value changes
 	useEffect(() => {
-		fetch('http://localhost:3000/trips')
+		fetch(url)
 			.then((response) => response.json())
 			.then((json) => setTrips(json));
-	}, []);
+	}, [url]); // * Rerun the new data
 	console.log(trips);
 
 	return (
@@ -25,6 +27,16 @@ export default function TripList() {
 					</li>
 				))}
 			</ul>
+			<div className='filters'>
+				<button
+					onClick={() => setUrl('http://localhost:3000/trips?loc=europe')}
+				>
+					European Trips
+				</button>
+				<button onClick={() => setUrl('http://localhost:3000/trips')}>
+					All Trips
+				</button>
+			</div>
 		</div>
 	);
 }
